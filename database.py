84c2a6b8375fc7c1a0d24087f2be9db953ce906b
xuoda <sem.mahalin@yandex.ru>
2024-11-23 21:52:37 +0300

    feat: fastAPI

--- database.py
+++ database.py
@@ -1,8 +1,12 @@
+from dotenv import load_dotenv
 import os
 
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+
+
+load_dotenv()
 
 USERNAME = os.getenv('USERNAME')
 PASSWORD = os.getenv('PASSWORD')
