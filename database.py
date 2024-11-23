b93f0ca1b320f99bb7b9ac7a9663f788cd109880
xuoda <sem.mahalin@yandex.ru>
2024-11-23 21:36:23 +0300

    feat: fastAPI

---
+++ database.py
@@ -0,0 +1,21 @@
+import os
+
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+USERNAME = os.getenv('USERNAME')
+PASSWORD = os.getenv('PASSWORD')
+DB_NAME = os.getenv('DB_NAME')
+
+DB_CREDENTIALS = {
+    'username': USERNAME,
+    'password': PASSWORD,
+    'db_name': DB_NAME,
+}
+SQLALCHEMY_DB = 'postgresql://{username}:{password}@localhost/{db_name}'.format(**DB_CREDENTIALS)
+
+engine = create_engine(SQLALCHEMY_DB)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
