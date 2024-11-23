9f08df06d8c0de39091d188ec4691139959bb601
Дорошенко Андрей Юрьевич <ayudoroshenko@phoenixit.ru>
2024-11-23 13:53:39 +0300

    fix(idea): залаваем билд и стабильность

--- front/nat-stup/src/pages/main-page.vue
+++ front/nat-stup/src/pages/main-page.vue
@@ -1,10 +1,52 @@
 <script setup lang="ts">
 import PersonForm from "@/widgets/person-form";
+import {onMounted, ref} from "vue";
+
+type PersonForm = {
+  direction: string;
+  applicantType: string;
+  regionOfProvision: string;
+  specificTags: string;
+}
+
+const inputValue = ref<PersonForm>({
+  direction: 'Гладить кота',
+  applicantType: 'Индивидуальный предприниматель',
+  regionOfProvision: 'ЮФО',
+  specificTags: 'шерсть',
+})
 
 function checkPerson(numberPerson: number){
   console.log(numberPerson)
+  if (numberPerson === 1) {
+    inputValue.value = {
+      direction: 'Гладить кота',
+      applicantType: 'Индивидуальный предприниматель',
+      regionOfProvision: 'ЮФО',
+      specificTags: 'шерсть',
+    }
+  } else if (numberPerson === 2) {
+    inputValue.value = {
+      direction: 'Чесать гуся',
+      applicantType: 'Физическое лицо',
+      regionOfProvision: 'Москва',
+      specificTags: 'перо',
+    }
+  } else {
+    inputValue.value = {
+      direction: 'Смотреть на стадо коров',
+      applicantType: 'Юридическое лицо',
+      regionOfProvision: 'Ростовская область',
+      specificTags: 'молоко',
+    }
+  }
+  console.log(inputValue.value)
 }
 
+onMounted(async () => {
+  const settingPerson = fetch('/settings/')
+  console.log(settingPerson)
+})
 </script>
 
 <template>
@@ -15,25 +57,30 @@ function checkPerson(numberPerson: number){
           class="user-info"
           @click="checkPerson(1)"
         >
-          <img alt="Vue logo" class="logo" src="../../../public/title-1.jpg" width="100" height="100" />
+          <img alt="Пользователь 1" class="logo" src="@/shared/assets/title-1.jpg" width="100" height="100" />
           Юзер-1
         </div>
         <div
           class="user-info"
           @click="checkPerson(2)"
         >
-          <img alt="Vue logo" class="logo" src="../../../public/title-2.jpg" width="100" height="100" />
+          <img alt="Пользователь 2" class="logo" src="@/shared/assets/title-2.jpg" width="100" height="100" />
           Юзер-2
         </div>
         <div
           class="user-info"
           @click="checkPerson(3)"
         >
-          <img alt="Vue logo" class="logo" src="../../../public/title-3.jpg" width="100" height="100" />
+          <img alt="Пользователь 3" class="logo" src="@/shared/assets/title-3.jpg" width="100" height="100" />
           Юзер-3
         </div>
       </div>
-      <person-form/>
+      <person-form
+        v-model:direction="inputValue.direction"
+        v-model:applicant-type="inputValue.applicantType"
+        v-model:region-of-provision="inputValue.regionOfProvision"
+        v-model:specific-tags="inputValue.specificTags"
+      />
     </div>
     <div class="stick-vertical"/>
     <div id="info-form">
