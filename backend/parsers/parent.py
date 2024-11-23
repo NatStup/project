b2529cd78aa708ba0216e7b89c3c8a90f8d51d3b
Paul Zaharov <paulgof@outlook.com>
2024-11-23 23:57:03 +0300

    feat(parser): add save method in parent class

--- backend/parsers/parent.py
+++ backend/parsers/parent.py
@@ -2,10 +2,13 @@ class Parser:
     def __init__(self, entrance_url):
         self.entrance_url = entrance_url
 
-    def scrape_item(self):
+    def save_item_in_file(self, item):
         pass
 
-    def scrape_page(self):
+    def scrape_item(self, item_id):
+        pass
+
+    def scrape_page(self, page_number):
         pass
 
     def scrape_pages(self):
